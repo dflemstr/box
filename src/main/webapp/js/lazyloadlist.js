2f835b41bd4d0ab7a56762c12fa3b0117a16794f
David Flemström <david.flemstrom@gmail.com>
2010-06-18 17:03:58 +0200

    Improved the lazy loader; it now checks for loader visibility when new content has arrived

--- src/main/webapp/js/lazyloadlist.js
+++ src/main/webapp/js/lazyloadlist.js
@@ -1,21 +1,22 @@
+var maybeLoadMore = function() {
+    var nativeWind = window,
+    wind = $(nativeWind),
+    loader = $('#lazy-loader').parent(),
+    margin = 200,
+    top = wind.scrollTop() - margin,
+    windJQHeight = wind.height(),
+    bottom = top + ((windJQHeight == 0) ? nativeWind.innerHeight : windJQHeight) + margin, //Height workaround for Chrome 5 and 6
+    loaderTop = loader.offset().top;
+    if(loaderTop > top && loaderTop < bottom) {
+        loader.trigger('loadmore');
+    }
+}
+
 var bindLoadEvent = function(toId, callback) {
     $('#' + toId).one('loadmore', callback);
+    maybeLoadMore();
 }
 
 $(function() {
-    var nativeWind = window,
-    wind = $(nativeWind),
-    maybeLoadMore = function() {
-        var loader = $('#lazy-loader').parent(),
-        margin = 200,
-        top = wind.scrollTop() - margin,
-        windJQHeight = wind.height(),
-        bottom = top + ((windJQHeight == 0) ? nativeWind.innerHeight : windJQHeight) + margin, //Height workaround for Chrome 5 and 6
-        loaderTop = loader.offset().top;
-        if(loaderTop > top && loaderTop < bottom) {
-            loader.trigger('loadmore');
-        }
-    };
-    wind.bind('scroll resize', maybeLoadMore);
-    $(maybeLoadMore);
+    $(window).bind('scroll resize', maybeLoadMore);
 });
