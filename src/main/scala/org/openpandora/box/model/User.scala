bf3b7b3fbec609d8c41adf573a656c2d4c145203
David Flemström <david.flemstrom@gmail.com>
2010-06-19 17:23:10 +0200

    Indentation fix

--- src/main/scala/org/openpandora/box/model/User.scala
+++ src/main/scala/org/openpandora/box/model/User.scala
@@ -41,7 +41,7 @@ case class User(@(Column @field)(length = 64)
   def gravatar = Helpers.hexEncode(Helpers.md5(email.toLowerCase.getBytes("UTF-8")))
 
   def gravatarImage(size: Int) =
-        <img src={"http://www.gravatar.com/avatar/" + gravatar + "?s=" + size + "&d=identicon"} alt={username} class="avatar"/>
+    <img src={"http://www.gravatar.com/avatar/" + gravatar + "?s=" + size + "&d=identicon"} alt={username} class="avatar"/>
 
   def login() {
     User._currentUser.remove()
