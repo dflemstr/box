977cc1e1f4aee7dd43c86158e8531d6ed77388c8
David Flemström <david.flemstrom@gmail.com>
2010-06-19 17:23:03 +0200

    Fix for a star-highlighting bug

--- src/main/webapp/js/ratings.js
+++ src/main/webapp/js/ratings.js
@@ -1,7 +1,9 @@
 $(function() {
-    $('a span.star').hover(function() {
-        $(this).addClass('star-hover').parent().prevAll().find('.star').addClass('star-hover');
-    }, function() {
-        $(this).removeClass('star-hover').parent().prevAll().find('.star').removeClass('star-hover');
+    var star = '.s', hover = 's-ho', selector = 'a span.s';
+    $(selector).live('mouseenter', function() {
+        $(this).addClass(hover).parent().prevAll().find(star).addClass(hover);
+    });
+    $(selector).live('mouseleave', function() {
+        $(this).removeClass(hover).parent().prevAll().find(star).removeClass(hover);
     });
 });
