7f1f48271126b265c3252c6ee741a9d3a1a34b65
David Flemström <david.flemstrom@gmail.com>
2010-06-19 22:36:40 +0200

    More stable search functionality

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,50 +1,18 @@
 $(function(){
     var orderingField = $('#ordering-field'),
-        directionField = $('#direction-field'),
-        searchField = $('#search-field'),
-        title = 'title',
-        rating = 'rating',
-        time = 'time',
-        def = 'default',
-        asc = 'asc',
-        desc = 'desc',
-        updateSearch = function() {
-            var d = directionField.val();
-            searchField.val((searchField.val().replace(/orderby:[a-z]+/, '').trim() +
-                ' orderby:' + orderingField.val() + ((d == def) ? '' : d)).trim());
-        },
-        search = searchField.val(),
-        ordering = search.match('orderby:([a-z]+)');
-    if(ordering) {
-        ordering = ordering[1];
-        if(ordering == title) {
-            orderingField.val(title);
-            directionField.val(def);
-        } else if(ordering == title + asc) {
-            orderingField.val(title);
-            directionField.val(asc);
-        } else if(ordering == title + desc) {
-            orderingField.val(title);
-            directionField.val(desc);
-        } else if(ordering == rating) {
-            orderingField.val(rating);
-            directionField.val(def);
-        } else if(ordering == rating + asc) {
-            orderingField.val(rating);
-            directionField.val(asc);
-        } else if(ordering == rating + desc) {
-            orderingField.val(rating);
-            directionField.val(desc);
-        } else if(ordering == time) {
-            orderingField.val(time);
-            directionField.val(def);
-        } else if(ordering == time + asc) {
-            orderingField.val(time);
-            directionField.val(asc);
-        } else if(ordering == time + desc) {
-            orderingField.val(time);
-            directionField.val(desc);
-        }
+    directionField = $('#direction-field'),
+    searchField = $('#search-field'),
+    def = 'default',
+    updateSearch = function() {
+        var d = directionField.val();
+        searchField.val((searchField.val().replace(/orderby:[a-z]+/, '').trim() +
+            ' orderby:' + orderingField.val() + ((d == def) ? '' : d)).trim());
+    },
+    search = searchField.val(),
+    ordersearch = search.match('orderby:(author|pxmlid|rating|time|title)(desc|asc|)');
+    if(ordersearch && ordersearch.length == 3) {
+        orderingField.val(ordersearch[1]);
+        directionField.val((ordersearch[2].length == 0)?def:ordersearch[2]);
     }
 
     orderingField.change(updateSearch);
