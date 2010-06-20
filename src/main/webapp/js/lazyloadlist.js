9863839944a52a191d06323e81f58c2e95e51b8f
David Flemström <david.flemstrom@gmail.com>
2010-06-20 04:12:11 +0200

    Lazyload fixes

--- src/main/webapp/js/lazyloadlist.js
+++ src/main/webapp/js/lazyloadlist.js
@@ -1,12 +1,14 @@
 var maybeLoadMore = function() {
-    var nativeWind = window,
-    wind = $(nativeWind),
-    loader = $('#lazy-loader').parent(),
-    windJQHeight = wind.height(),
-    windBottom = wind.scrollTop() + ((windJQHeight == 0) ? nativeWind.innerHeight : windJQHeight), //Height workaround for Chrome 5 and 6
-    loaderTop = loader.offset().top;
-    if(loaderTop < windBottom + 200) {
-        loader.trigger('loadmore');
+    var loader = $('#lazy-loader').parent();
+    if(loader && loader.offset()) {
+        var nativeWind = window,
+        wind = $(nativeWind),
+        windJQHeight = wind.height(),
+        windBottom = wind.scrollTop() + ((windJQHeight == 0) ? nativeWind.innerHeight : windJQHeight), //Height workaround for Chrome 5 and 6
+        loaderTop = loader.offset().top;
+        if(loaderTop < windBottom + 200) {
+            loader.trigger('loadmore');
+        }
     }
 }
 
