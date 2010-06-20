e72a85ac9bffc2a1c364416ba9d47b8d4a1b17fa
David Flemström <david.flemstrom@gmail.com>
2010-06-20 23:36:26 +0200

    Upgraded to squeryl-0.9.4beta6

--- project/build/BoxProject.scala
+++ project/build/BoxProject.scala
@@ -13,7 +13,7 @@ class BoxProject(info: ProjectInfo) extends DefaultWebProject(info) with hoffroc
 
   override def libraryDependencies = Set (
     "net.liftweb" % "lift-webkit" % liftVersion % "compile",
-    "org.squeryl" %% "squeryl" % "0.9.4beta5" % "compile",
+    "org.squeryl" %% "squeryl" % "0.9.4beta6" % "compile",
     "javax.servlet" % "servlet-api" % "2.4" % "provided",
     "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test",
     "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test",
