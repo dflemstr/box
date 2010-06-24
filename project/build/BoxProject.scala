37054a5cc32c7534c2108ab1aad062d531637a90
David Flemström <david.flemstrom@gmail.com>
2010-06-24 15:30:22 +0200

    Added Scalaz

--- project/build/BoxProject.scala
+++ project/build/BoxProject.scala
@@ -14,6 +14,7 @@ class BoxProject(info: ProjectInfo) extends DefaultWebProject(info) with hoffroc
   override def libraryDependencies = Set (
     "net.liftweb" % "lift-webkit" % liftVersion % "compile",
     "org.squeryl" %% "squeryl" % "0.9.4beta6" % "compile",
+    "com.googlecode.scalaz" %% "scalaz-core" % "5.0-M3-SNAPSHOT" % "compile",
     "javax.servlet" % "servlet-api" % "2.4" % "provided",
     "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test",
     "org.eclipse.jetty" % "jetty-webapp" % "7.0.2.RC0" % "test",
