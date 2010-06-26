d37bf936c7a94d86165f1c74d56ac018d1910b53
David Flemström <david.flemstrom@gmail.com>
2010-06-26 03:16:44 +0200

    Hotfix to clouds

--- src/main/scala/org/openpandora/box/snippet/ApplicationStatistics.scala
+++ src/main/scala/org/openpandora/box/snippet/ApplicationStatistics.scala
@@ -38,8 +38,8 @@ class ApplicationStatistics extends DispatchSnippet
 
   def categoryCloud(categoryCloud: NodeSeq): NodeSeq = {
     val counts: Map[Int, Long] = from(Database.categories)(category => groupBy(category.value) compute(count)).map(x => x.key -> x.measures)(collection.breakOut)
-    val min = counts.values.min.toDouble
-    val max = counts.values.max.toDouble
+    val min = if(counts.isEmpty) 0.0 else counts.values.min.toDouble
+    val max = if(counts.isEmpty) 0.0 else counts.values.max.toDouble
     counts.keys.flatMap {category =>
       val catName = DotDesktopCategories(category).toString
       <a href={"/applications/list?search=category:" + catName} class="category-cloud-element" style={"font-size:" + ((counts(category) - min) / (max - min) + 0.5) + "em;"}>{catName}</a>
@@ -48,8 +48,8 @@ class ApplicationStatistics extends DispatchSnippet
 
   def searchCloud(searchCloud: NodeSeq): NodeSeq = {
     val counts: Map[String, Long] = from(Database.searchKeywords)(keyword => groupBy(keyword.keyword) compute(count)).map(x => x.key -> x.measures)(collection.breakOut)
-    val min = counts.values.min.toDouble
-    val max = counts.values.max.toDouble
+    val min = if(counts.isEmpty) 0.0 else counts.values.min.toDouble
+    val max = if(counts.isEmpty) 0.0 else counts.values.max.toDouble
     counts.keys.flatMap {keyword =>
       <a href={"/applications/list?search=" + keyword} class="keyword-cloud-element" style={"font-size:" + ((counts(keyword) - min) / (max - min) + 0.5) + "em;"}>{keyword}</a>
     }.toSeq
