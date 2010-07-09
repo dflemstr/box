c553f051454d69dde4043e21499e3dcac578e366
David Flemström <david.flemstrom@gmail.com>
2010-07-09 12:54:08 +0200

    Added caching + features to Application

--- src/main/scala/org/openpandora/box/model/Application.scala
+++ src/main/scala/org/openpandora/box/model/Application.scala
@@ -16,6 +16,10 @@ case class Application(packageId:       Long, //id
                        versionBuild:    Int,
                        @Column(length = 512)
                        authorName:      Option[String],
+                       @Column(length = 512)
+                       authorURL:       Option[String],
+                       @Column(length = 512)
+                       authorEmail:       Option[String],
                        var ratingCount:     Long,
                        var ratingAverage:   Float,
                        val newest:       Boolean) extends LongKeyedEntity {
@@ -26,35 +30,55 @@ case class Application(packageId:       Long, //id
   lazy val metas:      OneToMany[AppMeta]  = Database.applicationsToAppMetas.left(this)
   lazy val ratings:    OneToMany[Rating]   = Database.applicationsToRatings.left(this)
 
+  lazy val commentsWithUsers: Query[(Comment, User)] =
+    from(Database.comments, Database.users)((comment, user) => where(comment.applicationId === id and comment.userId === user.id) select(comment, user))
+
+  lazy val categoriesSeq = categories.toSeq
+  lazy val commentsSeq   = comments.toSeq
+  lazy val metasSeq      = metas.toSeq
+
+  lazy val commentsWithUsersSeq = commentsWithUsers.toSeq
+
   final def pkg = `package` //alias
 
-  def this() = this(0l, "", 0, 0, 0, 0, Some(""), 0, 0f, false)
+  def version = versionMajor + "." + versionMinor + "." + versionBuild + "." + versionRelease
+
+  def hasRated(user: User) =
+    from(Database.ratings)(rating => where(rating.userId === user.id and rating.applicationId === id) compute(count)) > 0
+
+  def this() = this(0l, "", 0, 0, 0, 0, Some(""), Some(""), Some(""), 0, 0f, false)
 }
 
 object Application {
-  def apply(`package`: Package, pxmlId: String, versionMajor: Int, versionMinor: Int, versionRelease: Int, versionBuild: Int, authorName: Option[String]): Application = {
-    val countQuery = from(Database.applications)(app => where(
+  def updatesCount(pxmlId: String, major: Int, minor: Int, release: Int, build: Int): Long =
+    from(Database.applications)(app => where(
         app.pxmlId === pxmlId and (
           (
-            (app.versionMajor   gt  versionMajor)
+            (app.versionMajor   gt  major)
           ) or (
-            (app.versionMajor   === versionMajor) and
-            (app.versionMinor   gt  versionMinor)
+            (app.versionMajor   === major) and
+            (app.versionMinor   gt  minor)
           ) or (
-            (app.versionMajor   === versionMajor) and
-            (app.versionMinor   === versionMinor) and
-            (app.versionRelease gt  versionRelease)
+            (app.versionMajor   === major) and
+            (app.versionMinor   === minor) and
+            (app.versionRelease gt  release)
           ) or (
-            (app.versionMajor   === versionMajor) and
-            (app.versionMinor   === versionMinor) and
-            (app.versionRelease === versionRelease) and
-            (app.versionBuild   gt  versionBuild)
+            (app.versionMajor   === major) and
+            (app.versionMinor   === minor) and
+            (app.versionRelease === release) and
+            (app.versionBuild   gt  build)
           )
         )) compute(count))
-    val isNewest = ((countQuery: Long) == 0l)
+
+  def apply(`package`: Package, pxmlId: String,
+            versionMajor: Int, versionMinor: Int, versionRelease: Int, versionBuild: Int,
+            authorName: Option[String], authorURL: Option[String], authorEmail: Option[String]): Application = {
+    val isNewest = updatesCount(pxmlId, versionMajor, versionMinor, versionRelease, versionBuild) == 0l
 
     if(isNewest) update(Database.applications)(app => where(app.pxmlId === pxmlId) set(app.newest := false))
 
-    Application(`package`.id, pxmlId: String, versionMajor, versionMinor, versionRelease, versionBuild, authorName, 0, 0f, isNewest)
+    Application(`package`.id, pxmlId: String,
+                versionMajor, versionMinor, versionRelease, versionBuild,
+                authorName, authorURL, authorEmail, 0, 0f, isNewest)
   }
 }
